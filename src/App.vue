50004940ed2e1dc09501986a9ef25780841e0d88
Ammar Hassan <ammar@ammarhassan.co.uk>
2021-03-29 18:28:39 +0100

    styling complete

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <Homepage />
+  </div>
+</template>
+
+<script>
+import Homepage from "./components/Homepage.vue";
+
+export default {
+  name: "App",
+  components: {
+    Homepage,
+  },
+};
+</script>
+
+<style lang="scss">
+*{
+  margin: 0;
+  padding: 0;
+}
+html{
+  box-sizing: border-box;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Homepage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <div class="searchBox">
+      <div class="card">
+        <h1>Where are you going?</h1>
+        <form>
+          <label for="pickup">Pick-up Location</label>
+          <input
+            aria-labelledby="pick up location"
+            type="text"
+            name=""
+            id="pickup"
+            placeholder="city, airport, station, region, district ..."
+          />
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Homepage",
+  props: {
+    msg: String,
+  },
+};
+</script>
+<style lang="scss">
+@import url('./Homepage.scss');
+</style>
